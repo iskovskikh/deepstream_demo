291b4ebd9cab1b53e02c0e28f0c139cefc3e6343
Alexey Iskovskikh <iskovskikh@gmail.com>
2025-04-28 06:34:44 +0300

    upd

--- test_video_entrypoint.sh
+++ test_video_entrypoint.sh
@@ -12,11 +12,46 @@
 #  -rtsp_transport tcp \
 #  -f rtsp rtsp://mediamtx:8554/123
 
+#ffmpeg \
+#  -re \
+#  -f lavfi \
+#  -i testsrc=size=1920x1080:rate=60 \
+#  -vcodec libx264 \
+#  -pix_fmt yuv420p \
+#  -rtsp_transport tcp \
+#  -f rtsp rtsp://mediamtx:8554/test_input
+
+#ffmpeg \
+#  -re \
+#  -stream_loop -1 \
+#  -i $FILE_NAME \
+#  -tune zerolatency \
+#  -vcodec copy \
+#  -an \
+#  -rtsp_transport tcp \
+#  -f rtsp rtsp://mediamtx:8554/$STREAM_NAME-0 \
+#  -vcodec copy \
+#  -an \
+#  -rtsp_transport tcp \
+#  -f rtsp rtsp://mediamtx:8554/$STREAM_NAME-1 \
+#  -vcodec copy \
+#  -an \
+#  -rtsp_transport tcp \
+#  -f rtsp rtsp://mediamtx:8554/$STREAM_NAME-2
+#
+#
 ffmpeg \
   -re \
-  -f lavfi \
-  -i testsrc=size=1920x1080:rate=60 \
-  -vcodec libx264 \
-  -pix_fmt yuv420p \
+  -stream_loop -1 \
+  -i $FILE_NAME \
+  -tune zerolatency \
+  -vf "drawtext=fontfile=/usr/share/fonts/noto/NotoSans-Regular.ttf:text='Stack Overflow':fontcolor=white:fontsize=24:box=1:boxcolor=black@0.5:boxborderw=5:x=(w-text_w)/2:y=(h-text_h)/2,drawtext=fontfile=/usr/share/fonts/noto/NotoSans-Regular.ttf:text='Bottom right text':fontcolor=black:fontsize=14:x=w-tw-10:y=h-th-10" \
+  -an \
   -rtsp_transport tcp \
-  -f rtsp rtsp://mediamtx:8554/test_input
+  -f rtsp rtsp://mediamtx:8554/$STREAM_NAME-0 \
+  -an \
+  -rtsp_transport tcp \
+  -f rtsp rtsp://mediamtx:8554/$STREAM_NAME-1 \
+  -an \
+  -rtsp_transport tcp \
+  -f rtsp rtsp://mediamtx:8554/$STREAM_NAME-2
